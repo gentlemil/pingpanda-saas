37bae98e6104f68e018d3718baaf97516d02dab1
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-11-03 21:42:28 +0100

    add animated-list component from magic-ui lib, create discord-message component  and add animation into mock-discord-ui

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,6 +3,8 @@ import { MaxWidthWrapper } from '@/components/ui/max-width-wrapper'
 import { Check } from 'lucide-react'
 import { ShinyButton } from '@/components/ui/shiny-button'
 import { MockDiscordUI } from '@/components/ui/mock-discord-ui'
+import { AnimatedList } from '@/components/ui/animated-list'
+import { DiscordMessage } from '@/components/ui/discord-message'
 
 const Page = () => {
   return (
@@ -57,7 +59,52 @@ const Page = () => {
         <div className='relative mx-auto'>
           <MaxWidthWrapper className='relative'>
             <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
-              <MockDiscordUI />
+              <MockDiscordUI>
+                <AnimatedList>
+                  <AnimatedList>
+                    <DiscordMessage
+                      avatarSrc='/brand-asset-profile-picture.png'
+                      avatarAlt='PingPanda Avatar'
+                      username='PingPanda'
+                      timestamp='Today at 12:35PM'
+                      badgeText='SignUp'
+                      badgeColor='#43b581'
+                      title='👤 New user signed up'
+                      content={{
+                        name: 'Mateo Ortiz',
+                        email: 'm.ortiz19@gmail.com',
+                      }}
+                    />
+                    <DiscordMessage
+                      avatarSrc='/brand-asset-profile-picture.png'
+                      avatarAlt='PingPanda Avatar'
+                      username='PingPanda'
+                      timestamp='Today at 12:35PM'
+                      badgeText='Revenue'
+                      badgeColor='#faa61a'
+                      title='💰 Payment received'
+                      content={{
+                        amount: '$49.00',
+                        email: 'zoe.martinez2001@email.com',
+                        plan: 'PRO',
+                      }}
+                    />
+                    <DiscordMessage
+                      avatarSrc='/brand-asset-profile-picture.png'
+                      avatarAlt='PingPanda Avatar'
+                      username='PingPanda'
+                      timestamp='Today at 5:11AM'
+                      badgeText='Milestone'
+                      badgeColor='#5865f2'
+                      title='🚀 Revenue Milestone Achieved'
+                      content={{
+                        recurringRevenue: '$5.000 USD',
+                        growth: '+8.2%',
+                      }}
+                    />
+                  </AnimatedList>
+                </AnimatedList>
+              </MockDiscordUI>
             </div>
           </MaxWidthWrapper>
         </div>
