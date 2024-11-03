b7ad5cfdf47848be1150540f0902fad93fbe4985
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-11-03 20:49:30 +0100

    Create mock-discord-ui component (basic version without animation yet), add icons necessary for this project and create reusable icon component

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,6 +2,8 @@ import { Heading } from '@/components/ui/heading'
 import { MaxWidthWrapper } from '@/components/ui/max-width-wrapper'
 import { Check } from 'lucide-react'
 import { ShinyButton } from '@/components/ui/shiny-button'
+import { MockDiscordUI } from '@/components/ui/mock-discord-ui'
+
 const Page = () => {
   return (
     <>
@@ -50,7 +52,16 @@ const Page = () => {
           </div>
         </MaxWidthWrapper>
       </section>
-      <section></section>
+      <section className='relative bg-brand-25 pb-4'>
+        <div className='absolute inset-x-0 bottom-24 top-24 bg-brand-700' />
+        <div className='relative mx-auto'>
+          <MaxWidthWrapper className='relative'>
+            <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+              <MockDiscordUI />
+            </div>
+          </MaxWidthWrapper>
+        </div>
+      </section>
       <section></section>
       <section></section>
     </>
