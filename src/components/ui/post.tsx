684557b5cd75c046d22072f37874ee61e59c63a9
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-11-03 14:53:38 +0100

    Create Navbar component, install clerk and shadcn ui from where I get reusable button component, update imports after moving components to another folder and add middleware

--- src/components/ui/post.tsx
+++ src/components/ui/post.tsx
@@ -1,15 +1,15 @@
-"use client"
+'use client'
 
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { useState } from "react"
-import { client } from "../lib/client"
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
+import { client } from '../../app/lib/client'
 
 export const RecentPost = () => {
-  const [name, setName] = useState<string>("")
+  const [name, setName] = useState<string>('')
   const queryClient = useQueryClient()
 
   const { data: recentPost, isPending: isLoadingPosts } = useQuery({
-    queryKey: ["get-recent-post"],
+    queryKey: ['get-recent-post'],
     queryFn: async () => {
       const res = await client.post.recent.$get()
       return await res.json()
@@ -21,17 +21,17 @@ export const RecentPost = () => {
       await client.post.create.$post({ name })
     },
     onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: ["get-recent-post"] })
-      setName("")
+      await queryClient.invalidateQueries({ queryKey: ['get-recent-post'] })
+      setName('')
     },
   })
 
   return (
-    <div className="w-full max-w-xs">
+    <div className='w-full max-w-xs'>
       {isLoadingPosts ? (
         <p>Loading posts...</p>
       ) : recentPost ? (
-        <p className="truncate">Your most recent post: {recentPost.name}</p>
+        <p className='truncate'>Your most recent post: {recentPost.name}</p>
       ) : (
         <p>You have no posts yet.</p>
       )}
@@ -40,18 +40,18 @@ export const RecentPost = () => {
           e.preventDefault()
           createPost.mutate({ name })
         }}
-        className="flex flex-col gap-2"
+        className='flex flex-col gap-2'
       >
         <input
-          type="text"
-          placeholder="Enter a title..."
+          type='text'
+          placeholder='Enter a title...'
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className="w-full rounded-md outline outline-2 outline-brand-700 h-12 px-4 py-2 text-black"
+          className='w-full rounded-md outline outline-2 outline-brand-700 h-12 px-4 py-2 text-black'
         />
         <button
-          type="submit"
-          className="rounded-md h-12 px-10 py-3 bg-brand-700 text-brand-50 font-semibold transition hover:bg-brand-800"
+          type='submit'
+          className='rounded-md h-12 px-10 py-3 bg-brand-700 text-brand-50 font-semibold transition hover:bg-brand-800'
         >
           Submit
         </button>
