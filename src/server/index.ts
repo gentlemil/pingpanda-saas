c48be4840b3b06351c2417314376ec54f4a7255a
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-11-04 20:36:58 +0100

    Make first API call by adding new user to the db after register with clerk and redirect to dashboard page

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,16 @@
-import { Hono } from "hono"
-import { cors } from "hono/cors"
-import { handle } from "hono/vercel"
-import { postRouter } from "./routers/post-router"
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { handle } from 'hono/vercel'
+import { authRouter } from './routers/auth-router'
 
-const app = new Hono().basePath("/api").use(cors())
+const app = new Hono().basePath('/api').use(cors())
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /server/routers should be manually added here.
  */
-const appRouter = app.route("/post", postRouter)
+const appRouter = app.route('/auth', authRouter)
 
 // The handler Next.js uses to answer API requests
 export const httpHandler = handle(app)
