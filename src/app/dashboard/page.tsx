d1411a8a6509e83706504ad058cdf2117a866342
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-11-04 21:31:41 +0100

    Create Dashboard-page wrapper

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,15 @@
-const Page = () => {
-  return <div>Dashboard</div>
+import { DashboardPage } from '@/components/dashboard-page'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+  const auth = await currentUser()
+
+  if (!auth) {
+    redirect('/sign-in')
+  }
+
+  return <DashboardPage title='Dashboard'>Content</DashboardPage>
 }
 
 export default Page
---
+++ src/components/dashboard-page.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { ReactNode } from 'react'
+import { Button } from '@/components/ui/button'
+import { Heading } from '@/components/heading'
+import { ArrowLeft } from 'lucide-react'
+
+interface DashboardPageProps {
+  title: string
+  children?: ReactNode
+  hideBackButton?: boolean
+  cta?: ReactNode
+}
+
+export const DashboardPage = ({
+  title,
+  children,
+  hideBackButton,
+  cta,
+}: DashboardPageProps) => {
+  const router = useRouter()
+  return (
+    <section className='flex-1 h-full w-full flex flex-col'>
+      <div className='p-6 sm:p-8 flex justify-between border-b border-gray-200'>
+        <div className='w-full flex flex-col sm:flex-row items-start sm:items-center gap-6'>
+          <div className='flex items-center gap-8'>
+            {!hideBackButton && (
+              <Button
+                onClick={() => router.push('/dashboard')}
+                className='w-fit bg-white'
+                variant='outline'
+              >
+                <ArrowLeft className='size-4' />
+              </Button>
+            )}
+            <Heading>{title}</Heading>
+          </div>
+
+          {cta && <div className='w-full'>{cta}</div>}
+        </div>
+      </div>
+
+      <div className='flex-1 p-6 sm:p-8 flex flex-col overflow-y-auto'>
+        {children}
+      </div>
+    </section>
+  )
+}
